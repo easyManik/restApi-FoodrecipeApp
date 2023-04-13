bc95a61f08378d45667aef868cf0f45070b40d4e
Easy Destini Manik <72480830+easyManik@users.noreply.github.com>
2023-04-13 12:33:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = 3000;
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:3001",
+  origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
